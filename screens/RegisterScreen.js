86989a061f8b915eb4b40f6a987ac2cae0716eea
Mustafa TOPAL <Mustafa.Topal@Plastifay.com.tr>
2024-03-11 03:38:31 +0300

    Updated

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,11 +1,223 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  KeyboardAvoidingView,
+  Image,
+  SafeAreaView,
+  TextInput,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialIcons, AntDesign, Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
 const RegisterScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+  const handleRegister = () => {
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+    };
+    //send a post request to the backend API
+    try {
+      axios
+        .post(
+          "http://192.168.1.105:3000/register",
+          user,
+          (headers = { "Content-Type": "application/json" })
+        )
+        .then((response) => {
+          console.log(response);
+          Alert.alert(
+            "Registration Succesfull",
+            "You have registered successfully"
+          );
+          setName("");
+          setEmail("");
+          setPassword("");
+        })
+        .catch((error) => {
+          Alert.alert(
+            "Registration Error",
+            "An error occured during registration"
+          );
+          console.log("Registration Failed", error);
+        });
+    } catch (error) {
+      Alert.alert("Database", error);
+    }
+  };
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "white", alignItems: "center" }}
+    >
+      <View>
+        <Image
+          style={{ width: 150, height: 150, marginTop: 50 }}
+          source={{
+            uri: "https://assets.stickpng.com/thumbs/6160562276000b00045a7d97.png",
+          }}
+        />
+      </View>
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: "bold",
+              marginTop: 12,
+              color: "#041E42",
+            }}
+          >
+            Register To Your Account
+          </Text>
+        </View>
+        <View style={{ marginTop: 20 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 5,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <Ionicons
+              style={{ marginLeft: 8 }}
+              name="person"
+              size={24}
+              color="gray"
+            />
+            <TextInput
+              value={name}
+              onChangeText={(text) => setName(text)}
+              style={{
+                color: "gray",
+                marginVertical: 10,
+                width: 300,
+                fontSize: name ? 16 : 16,
+              }}
+              placeholder="Enter Your Name"
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            backgroundColor: "#D0D0D0",
+            paddingVertical: 5,
+            borderRadius: 5,
+            marginTop: 30,
+          }}
+        >
+          <MaterialIcons
+            style={{ marginLeft: 8 }}
+            name="email"
+            size={24}
+            color="gray"
+          />
+          <TextInput
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            style={{
+              color: "gray",
+              marginVertical: 10,
+              width: 300,
+              fontSize: email ? 16 : 16,
+            }}
+            placeholder="Enter Your Email"
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            backgroundColor: "#D0D0D0",
+            paddingVertical: 5,
+            borderRadius: 5,
+            marginTop: 30,
+          }}
+        >
+          <AntDesign
+            style={{ marginLeft: 8 }}
+            name="lock"
+            size={24}
+            color="gray"
+          />
+          <TextInput
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
+            style={{
+              color: "gray",
+              marginVertical: 10,
+              width: 300,
+              fontSize: password ? 16 : 16,
+            }}
+            placeholder="Enter Your Pasword"
+          />
+        </View>
+
+        <View
+          style={{
+            marginTop: 12,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text>Keep me Logged In</Text>
+          <Text style={{ color: "#007FFF", fontWeight: 500 }}>
+            Forget Password
+          </Text>
+        </View>
+        <View style={{ marginTop: 80 }}>
+          <Pressable
+            onPress={() => handleRegister()}
+            style={{
+              width: 200,
+              backgroundColor: "#FEBE10",
+              borderRadius: 6,
+              marginLeft: "auto",
+              marginRight: "auto",
+              padding: 15,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+                fontSize: 16,
+                fontWeight: "bold",
+              }}
+            >
+              Register
+            </Text>
+          </Pressable>
+        </View>
+        <Pressable
+          onPress={() => navigation.goBack()}
+          style={{ marginTop: 15 }}
+        >
+          <Text style={{ textAlign: "center", color: "gray", fontSize: 16 }}>
+            Already have an account? Sign In!
+          </Text>
+        </Pressable>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
